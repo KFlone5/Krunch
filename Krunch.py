4ce82c91c081b8e99c504dce8e97fafc30bb8337
KFlone5 <kirilleasy102030@gmail.com>
2026-03-16 00:21:58 +0400

    CLI added

--- Krunch.py
+++ Krunch.py
@@ -4,7 +4,29 @@ UPP_CHARSET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 NUM_CHARSET = "0123456789"
 SYM_CHARSET = "!@#$%^&*()-_+=~`[]{}|\\:;\"'<>,.?/"
 
-def wordlist_generator(pattern, strings, output_file="out.txt"):
+def print_welcome():
+    print("""$$\   $$\                                         $$\       
+$$ | $$  |                                        $$ |      
+$$ |$$  /  $$$$$$\  $$\   $$\ $$$$$$$\   $$$$$$$\ $$$$$$$\  
+$$$$$  /  $$  __$$\ $$ |  $$ |$$  __$$\ $$  _____|$$  __$$\ 
+$$  $$<   $$ |  \__|$$ |  $$ |$$ |  $$ |$$ /      $$ |  $$ |
+$$ |\$$\  $$ |      $$ |  $$ |$$ |  $$ |$$ |      $$ |  $$ |
+$$ | \$$\ $$ |      \$$$$$$  |$$ |  $$ |\$$$$$$$\ $$ |  $$ |
+\__|  \__|\__|       \______/ \__|  \__| \_______|\__|  \__|""")
+    
+    print("""
+Pattern Symbols
+-----------------------------------------------------------------------\\
+| @  | Characters from your custom charset (-s)                        |
+| %  | Digits 0-9                                                      |
+| ^  | Special characters accessed via Shift + number key !@#$%^&*()   |
+| !  | All letters, both uppercase and lowercase (a-z A-Z)             |
+| $  | Lowercase letters only (a-z)                                    |
+| &  | Uppercase letters only (A-Z)                                    |
+-----------------------------------------------------------------------/    
+\n\n""")
+
+def wordlist_generator(pattern, strings, output_file):
     charsets = []
 
     for char in pattern:
@@ -34,9 +56,23 @@ def wordlist_generator(pattern, strings, output_file="out.txt"):
         generate(0, "")
 
 def main():
-    pattern = "AIS@%"
-    strings = "Aa5#"
-    wordlist_generator(pattern, strings, "wordlist.txt")
+    print_welcome()
+    charset = ""
+    
+    print("Please enter the pattern of the password")
+    pattern = input(">>>")
+    
+    if '@' in pattern:
+        print("Please enter the charset")
+        charset = input(">>>")
+    
+    print("Enter the filename where the wordlist will be written (out.txt by default)")
+    output_file = input(">>>")
+
+    if output_file == "":
+        output_file = "out.txt"
+    
+    wordlist_generator(pattern, charset, output_file)
 
 if __name__ == "__main__":
     main()
